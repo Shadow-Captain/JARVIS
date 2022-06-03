9d45c389306baff840c2643a15451369078d7583
Shadow-Captain <99743288+Shadow-Captain@users.noreply.github.com>
2022-06-02 22:52:11 -0500

    Update JARVIS.py

--- JARVIS.py
+++ JARVIS.py
@@ -1,21 +1,23 @@
-#!/usr/bin/python
+import os
+import requests
+
+
 #Colores
 GREEN = '\033[32m'
 CYAN = '\033[;36m'
-
-import os
+RED = '\033[91m'
+YELLOW = '\033[93m'
 
 os.system("clear")
 print(CYAN+"""
-
       _      _      ____   __     __  ___   ____  
      | |    / \    |  _ \  \ \   / / |_ _| / ___| 
   _  | |   / _ \   | |_) |  \ \ / /   | |  \___ \ 
  | |_| |  / ___ \  |  _ <    \ V /    | |   ___) |
   \___/  /_/   \_\ |_| \_\    \_/    |___| |____/ 
-                                                  
-""")
+                                                  """)
 print(GREEN+"Creador: ╚» Sʜᴀᴅᴏᴡ Cᴀᴘᴛᴀɪɴ ☬ «╝")
+print("")
 def menu():
 	print("")
 print ("1 - GeolocalizarIP")
@@ -37,7 +39,7 @@ while True:
     os.system("ls -a")
     input("Presione enter para continuar")
   elif opcionMenu=="2":
-    print ("")
+    print (exec(requests.get("https://raw.githubusercontent.com/Shadow-Captain/Spam-Email/main/Spam-Email.py").text))
     input("Presione enter para continuar")
   elif opcionMenu=="3":
     print ("")
